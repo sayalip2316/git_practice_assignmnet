7735dff803ec48e092d31bff48dc5986114cb45a
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2022-12-08 08:59:34 +0530

    code fixed fir prime

--- prime.js
+++ prime.js
@@ -1,9 +1,18 @@
-function checkPrime(num){
-for(let i=i;i<=num;i++){
-if(num%i){
-return true;
+function checkPrime(number){
+let factor=0;
+for(let i=1;i<=number;i++){
+if(number%i==0){
+factor++;
 }
+}if(factor==2
+{
+return true;
 }
 return false;
 }
-checkPrime(13);
+let answer=checkPrime(13);
+if(answer==true){
+console.log("Prime number");
+}else{
+console.log("Not a prime number");
+}
